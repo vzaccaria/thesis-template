cf082c89f7e7b4d373e8de9256b1a15935c9a483
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2019-07-29 11:21:25 +0200

    update: minor changes (makefile.mk, data.tex)

--- makefile.mk
+++ makefile.mk
@@ -1,8 +1,17 @@
 
 all: cover.pdf thesis.pdf
 
-%.pdf: %.tex src/preamble.tex 
-	jslatex $<
+cover.pdf: cover.tex src/preamble.tex 
+	pdflatex -interaction=nonstopmode -halt-on-error -shell-escape $< > latex.log
+
+thesis.pdf: thesis.tex src/preamble.tex 
+	pdflatex -interaction=nonstopmode -halt-on-error -shell-escape $< > latex.log
+	bibtex thesis >> latex.log
+	pdflatex -interaction=nonstopmode -halt-on-error -shell-escape $<  >> latex.log
+	pdflatex -interaction=nonstopmode -halt-on-error -shell-escape $< | egrep "arning|rror|ages"
+
+complete.pdf: cover.pdf thesis.pdf
+	pdftk $^ cat output $@
 
 clean: 
-	rm -f *.pdf *.lof *.lol
+	@rm -f *.toc *.aux *.bbl *.blg *.dvi *.bak *.ps *bkup *.tcp *.tps *.log *.lof *.lot *.lol *~ *.backup *.pdf
