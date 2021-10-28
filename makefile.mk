80c46b5a9298275071ba3611e7744f3e8904f9e5
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2021-10-28 11:09:32 +0200

    update: minor changes (makefile.mk, preamble.tex, thesis.tex)

--- makefile.mk
+++ makefile.mk
@@ -1,9 +1,6 @@
-SRC?= $(wildcard src/*.tex src/chapters/*.tex src/frontmatter/*.tex)
+SRC?= $(wildcard src/*.tex src/chapters/*.tex)
 
-all: complete.pdf 
-
-cover.pdf: cover.tex $(SRC)
-	pdflatex -interaction=nonstopmode -halt-on-error -shell-escape $< > latex.log
+all: thesis.pdf 
 
 thesis.pdf: thesis.tex  $(SRC)
 	pdflatex -interaction=nonstopmode -halt-on-error -shell-escape $< > latex.log
@@ -11,11 +8,9 @@ thesis.pdf: thesis.tex  $(SRC)
 	pdflatex -interaction=nonstopmode -halt-on-error -shell-escape $<  >> latex.log
 	pdflatex -interaction=nonstopmode -halt-on-error -shell-escape $< | egrep "arning|rror|ages"
 
-complete.pdf: cover.pdf thesis.pdf
-	pdftk $^ cat output $@
 
-example.pdf: complete.pdf
+example.pdf: thesis.pdf
 	cp $< $@
 
 clean: 
-	@rm -f *.toc *.aux *.bbl *.blg *.dvi *.bak *.ps *bkup *.tcp *.tps *.log *.lof *.lot *.lol *~ *.backup complete.pdf cover.pdf
+	@rm -f *.toc *.aux *.bbl *.blg *.dvi *.bak *.ps *bkup *.tcp *.tps *.log *.lof *.lot *.lol *~ *.backup thesis.pdf example.pdf
